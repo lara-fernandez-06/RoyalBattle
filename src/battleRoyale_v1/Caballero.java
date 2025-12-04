21cd61620034d89500e713412190a8d3b288b9bf
lara-rpg <lara.fsanchez@gmail.com>
2025-12-04 16:49:29 +0100

    minor changes prueba

--- src/battleRoyale_v1/Caballero.java
+++ src/battleRoyale_v1/Caballero.java
@@ -2,4 +2,28 @@ package battleRoyale_v1;
 
 public class Caballero extends Personaje {
 
+	@Override
+	public void atacar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quitarVida(int dmg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean checkBuff() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void checkSurroundings() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
