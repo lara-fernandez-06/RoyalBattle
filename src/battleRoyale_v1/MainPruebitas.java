e36e77c72d0ba43c52ccfff80b9ae77fe7a70304
ceciarmyoficial <cec.munozdom@gmail.com>
2025-12-09 16:17:58 +0100

    el chiste

---
+++ src/battleRoyale_v1/MainPruebitas.java
@@ -0,0 +1,12 @@
+package battleRoyale_v1;
+
+public class MainPruebitas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PruebasAlonso pa = new PruebasAlonso();
+		
+		pa.imprimirMensajeAlonso();
+	}
+
+}
