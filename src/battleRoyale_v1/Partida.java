e0c1c72672e4c8a728c8342c6713c7a0bd1e30ef
SunDrake153 <alonsomail05@gmail.com>
2025-12-11 01:59:45 +0100

    El Usuario Elige Un Personaje
    Estoy cansado y no consigo apagar la luz de mi habitación y mañana hay examen de mates. Porque todo va tan mal, me voy ha poner a llorar

--- src/battleRoyale_v1/Partida.java
+++ src/battleRoyale_v1/Partida.java
@@ -8,14 +8,74 @@ public class Partida {
 	private ArrayList<Personaje> jugadores;
 	private ArrayList<ArrayList<Personaje>> tablero;
 	
-	public void jugarPartida() {
-		//jugadores.add(crearPersonajeUsuario());
+	public Partida() {
+		jugadores = new ArrayList<Personaje>();
+		tablero = new ArrayList<ArrayList<Personaje>>();
 	}
 	
-	/*
+	public void jugarPartida() {
+		jugadores.add(crearPersonajeUsuario());
+		System.out.println(jugadores.getFirst().toString());
+	}
+	
+	
 	private Personaje crearPersonajeUsuario() {
 		Scanner escaner = new Scanner(System.in);
+		String rol;
+		boolean error = false;
+		
+		do {
+			System.out.println("Elige un personaje (Caballero, Ogro, Elfo, Mago, Ladrón): ");
+			rol = escaner.nextLine();
+			if(rol.equalsIgnoreCase("Caballero")) {
+				return new Caballero(escogerNombre(escaner), escogerArma(escaner));
+			}else if(rol.equalsIgnoreCase("Ogro")) {
+				return new Ogro(escogerNombre(escaner), escogerArma(escaner));
+			}else if(rol.equalsIgnoreCase("Elfo")) {
+				return new Elfo(escogerNombre(escaner), escogerArma(escaner));
+			}else if(rol.equalsIgnoreCase("Mago")) {
+				return new Mago(escogerNombre(escaner), escogerArma(escaner));
+			}else if(rol.equalsIgnoreCase("Ladrón")) {
+				return new Ladron(escogerNombre(escaner), escogerArma(escaner));
+			}else {
+				System.err.println("Error, no has escrito ninguno de los roles disponibles.Intentalo de nuevo.");
+				error = true;
+			}
+		}while(error == true);
+		
+		return null;
 		
 	}
-	*/
+	
+	private String escogerNombre(Scanner escaner) {
+		System.out.println("Escribe el nombre de tu personaje:");
+		return escaner.nextLine();
+	}
+	
+	private Arma escogerArma(Scanner escaner) {
+		//definimos las variables
+		String arma;
+		boolean error = false;
+		
+		do {
+			System.out.println("Escoge un arma(Espada, Maza, Arco, Báculo, Daga): ");
+			arma = escaner.nextLine();
+			if(arma.equalsIgnoreCase("Espada")) {
+				return new Espada();
+			}else if(arma.equalsIgnoreCase("Maza")) {
+				return new Maza();
+			}else if(arma.equalsIgnoreCase("Arco")) {
+				return new Arco();
+			}else if(arma.equalsIgnoreCase("Báculo")) {
+				return new Baculo();
+			}else if(arma.equalsIgnoreCase("Daga")) {
+				return new Daga();
+			}else {
+				System.err.println("Error, no has escrito ninguno de los roles disponibles.Intentalo de nuevo.");
+				error = true;
+			}
+		}while(error == true);
+		
+		return null;
+	}
 }
