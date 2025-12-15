0308038c3a29b1164f045498e05753c015b45057
SunDrake153 <alonsomail05@gmail.com>
2025-12-15 21:13:15 +0100

    definitivo
    definitivo

--- src/battleRoyale_v1/Main.java
+++ src/battleRoyale_v1/Main.java
@@ -3,7 +3,6 @@ package battleRoyale_v1;
 public class Main {
 
 	public static void main(String[] args) {
-		//Partida p = new Partida();
 		Settings setting = new Settings();
 		
 		//p.jugarPartida();
